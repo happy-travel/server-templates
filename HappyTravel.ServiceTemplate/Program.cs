db04c7f01c7b6f950ba7abe8c5f354797471dbe0
Kirill Taran <kirill.taran@hotmail.com>
2019-07-28 23:14:06 +0300

    Add project files.

---
+++ HappyTravel.ServiceTemplate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using HappyTravel.SentryLogger.Extensions;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace HappyTravel.ServiceTemplate
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateWebHostBuilder(args)
+                .Build()
+                .RunAsync();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var environment = hostingContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.ClearProviders()
+                        .AddConfiguration(hostingContext.Configuration.GetSection("Logging"))
+                        .AddSentry(c =>
+                        {
+                            c.Endpoint = hostingContext.Configuration["Logging:Sentry:Endpoint"];
+                        });
+
+                    var env = hostingContext.HostingEnvironment;
+                    if (env.IsDevelopment())
+                        logging.AddConsole();
+                    else
+                        logging.AddEventSourceLogger();
+                });
+    }
+}
---
+++ HappyTravel.ServiceTemplate/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HappyTravel.ServiceTemplate.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HappyTravel.ServiceTemplate
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+            services.AddHealthChecks();
+
+            services.AddHostedService<Host>();
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseHealthChecks("/health");
+        }
+
+
+        private static string GetFromEnvironment(IConfiguration configuration, string key)
+        {
+            var environmentVariable = configuration[key];
+            if (environmentVariable is null)
+                throw new Exception($"Couldn't obtain the value for '{key}' configuration key.");
+
+            return Environment.GetEnvironmentVariable(environmentVariable);
+        }
+    }
+}
